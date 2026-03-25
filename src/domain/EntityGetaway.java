6a01ca4fc30e56c08d9318b1b3fb79882feac6d0
Heiddeggenerate <chokoladk00@gmail.com>
2026-03-25 13:07:29 +1000

    CREATE: EntityGetaway + Reader

---
+++ src/domain/EntityGetaway.java
@@ -0,0 +1,5 @@
+public interface EntityGetaway {
+
+    public String readLine();
+
+}
---
+++ src/infrastructure/Reader.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Reader implements AutoCloseable, Getaway {
+
+    private final BufferedReader reader;
+
+    public Reader(String path) throws FileNotFoundException {
+        reader = new BufferedReader(new FileReader(path));
+    }
+
+    @Override
+    public String readLine() {
+        try {
+            return reader.readLine();
+        } catch (IOException ex) {
+            throw new RuntimeException("Ошибка чтения строки", ex);
+        }
+    }
+
+    @Override
+    public void close() {
+        try {
+            reader.close();
+        } catch (IOException ex) {
+            throw new RuntimeException("Ошибка закрытия файла чтения", ex);
+        }
+    }
+}

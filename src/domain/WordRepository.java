082774482cb7998c3f1e2cee908231d11280def7
Heiddeggenerate <chokoladk00@gmail.com>
2026-03-25 14:19:28 +1000

    UPDATE: WordRepository created count to word link

--- src/domain/WordRepository.java
+++ src/domain/WordRepository.java
@@ -1,13 +1,31 @@
 package domain;
 
-import java.util.Set;
-import java.util.TreeMap;
+import java.util.*;
 
 public class WordRepository {
 
     private final TreeMap<String, Integer> wordCount = new TreeMap<>();
+    private final TreeMap<Integer, List<String>> countWords = new TreeMap<>(COMPARE_BY_SIZE);
 
     public void insert(String word, int count) {
+        wordCountInsert(word, count);
+        countWordsInsert(word, count);
+    }
+
+    private void countWordsInsert(String word, int count) {
+        int insideCount = getCount(word);
+        int prevPos = insideCount - count;
+        if (countWords.get(insideCount) == null)
+            countWords.put(insideCount, new ArrayList<>());
+        if (countWords.get(prevPos) != null) {
+            countWords.get(prevPos).remove(word);
+            if (countWords.get(prevPos).isEmpty())
+                countWords.remove(prevPos);
+        }
+        countWords.get(insideCount).add(word);
+    }
+
+    private void wordCountInsert(String word, int count) {
         int sum = 0;
         if (wordCount.get(word) != null)
             sum += wordCount.get(word);
@@ -22,8 +40,22 @@ public class WordRepository {
         return wordCount.get(word);
     }
 
-    public Set<String> getKeys() {
+    public List<String> getWords(int count) {
+        return countWords.get(count);
+    }
+
+    public Set<String> getWordsKeys() {
         return wordCount.keySet();
     }
 
+    public Set<Integer> getCountKeys() {
+        return countWords.keySet();
+    }
+
+    public static Comparator<Integer> COMPARE_BY_SIZE = new Comparator<Integer>() {
+        @Override
+        public int compare(Integer o1, Integer o2) {
+            return o2 - o1;
+        }
+    };
 }
